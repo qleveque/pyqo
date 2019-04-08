922b7fd7f784720b86081002533887712574c986
quentin leveque <qleveque@hotmail.com>
2019-04-08 19:57:21 +0200

    first update on linux

--- setup.py
+++ setup.py
@@ -7,13 +7,29 @@ from _write_scripts import *
 current_path = os.path.dirname(os.path.abspath(__file__))
 write_scripts()
 
+def addLines(filename, content, add):
+	with open(filename, "r+") as file:
+		for line in file:
+			if content in line:
+				return
+		file.write(add)
+
 if sys.platform in ['linux', 'linux2']:
-    pass
+	#profile
+	content_flag = "#PYQ_START"
+
+	add_profile = """
+#PYQ_START
+PATH="{}:$PATH"
+alias c='source c'
+#PYQ_END
+""".format(os.path.join(current_path,'scripts'))
+	addLines("{}/.profile".format(os.environ['HOME']), content_flag, add_profile)
+
 else:
-    #TODO
-    #pyq = os.path.join(current_path, 'batches')
-    #subprocess.call('setx /M PATH "%PATH%;{}"'.format(pyq), shell=True)
-    #config = os.path.join(current_path, 'windows_config')
-    #subprocess.call('setx /M PATH "%PATH%;{}"'.format(config), shell=True)
-    pass
-    
+	#TODO
+	#pyq = os.path.join(current_path, 'batches')
+	#subprocess.call('setx /M PATH "%PATH%;{}"'.format(pyq), shell=True)
+	#config = os.path.join(current_path, 'windows_config')
+	#subprocess.call('setx /M PATH "%PATH%;{}"'.format(config), shell=True)
+	pass
