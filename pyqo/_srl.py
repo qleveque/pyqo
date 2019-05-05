c7d50cd265dec2870146921dfbc512285243bf2e
quentin leveque <qleveque@hotmail.com>
2019-05-05 12:16:04 +0200

    remove becomes delete

--- pyqo/_srl.py
+++ pyqo/_srl.py
@@ -2,7 +2,7 @@
 """
     The ``_srl`` module
     ===================
-    Contains all the functions related to the SRL ("set", "remove" and "list") commands.
+    Contains all the functions related to the SRL ("set", "delete" and "list") commands.
 """
 
 import click
@@ -10,12 +10,12 @@ from ._json import *
 
 def decorate_srl(f):
     return click.option('--assign', '-a', help='Assign a value to the given key.', default=None,)(
-    click.option('--remove', '-r', help='Remove a key.', is_flag=True,)(
+    click.option('--delete', '-d', help='Delete a key.', is_flag=True,)(
     click.option('--list', '-l', help='List all the keys if no given key. Otherwise print the associated value.', is_flag=True,)(
     click.option('--set_datafile', '-s', help='Set considered datafile for this command.', default=None,)(
     f))))
 
-def handle_srl(command, filename, keys, assign = None, remove = None, list = None, set_datafile = None, type = None):
+def handle_srl(command, filename, keys, assign = None, delete = None, list = None, set_datafile = None, type = None):
     if assign is not None:
         if len(keys)!=1:
             print("When setting a new value, you should provide exactly one key.")
@@ -25,14 +25,14 @@ def handle_srl(command, filename, keys, assign = None, remove = None, list = Non
         set_json(filename, {keys[0]: assign})
         return True
 
-    if remove:
+    if delete:
         remove_json(filename, keys)
         return True
 
     if list:
         list_json(filename, keys)
         return True
-        
+
     if set_datafile is not None:
         if len(keys)>0:
             print('Should not provide any key when setting a new datafile')
